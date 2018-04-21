9f7cedf8c4eed566862d5522ee2e21c3496e9c86
Przemysław Cieślarek <madflow12@gmail.com>
2018-04-21 18:52:33 +0200

    4 update

--- Program4/src/com/prog4/Program4.java
+++ Program4/src/com/prog4/Program4.java
@@ -1,19 +1,27 @@
 package com.prog4;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program4 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-
-
+        int length = 0;
+        int depth= 0;
         Scanner input = new Scanner(System.in);
 
+        try {
+            System.out.println("Enter length: ");
+            length = input.nextInt();
+            System.out.println("Enter depth: ");
+            depth = input.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("Error: "+e);
+        }
 
-        int nInches     =  3;
-        int majorLength =  5;
+        input.close();
         Ruler ruler = new Ruler();
-        ruler.drawRuler( nInches, majorLength );
+        ruler.drawRuler(length, depth);
     }
 }
--- Program4/src/com/prog4/Ruler.java
+++ Program4/src/com/prog4/Ruler.java
@@ -2,33 +2,40 @@ package com.prog4;
 
 public class Ruler {
 
-    public static void drawRuler(int centimeter, int length){
-        drawLine(length, 0);
-        for(int i = 1; i <= centimeter; i++){
-            drawInterval(length - 1);
-            drawLine(length, i);
+    public void drawRuler(int length, int depth) {
+        drawLine(depth, 0);
+        for (int i = 1; i <= length; i++) {
+            drawInterval(depth - 1);
+            drawLine(depth, i);
         }
     }
 
-    private static void drawInterval(int centralLength){
-        if(centralLength >= 1){
-            drawInterval(centralLength -1);
-            drawLine(centralLength);
-            drawInterval(centralLength - 1);
+    /**
+     * Rysuje lninie od punktu srodkowego
+     */
+    private void drawInterval(int center) {
+        if (center >= 1) {
+            drawInterval(center - 1);
+            drawLine(center);
+            drawInterval(center - 1);
         }
     }
 
-    private static void drawLine(int tickLength, int tickLabel){
-        for(int i = 0; i < tickLength; i++){
-            System.out.println("-");
-            }
-            if(tickLabel >= 0){
-                System.out.println(" " +tickLabel);
-                System.out.println("\n");
-            }
+    /**
+     * Rysuje linie o podanej dlugosci z opcjonalna etykieta
+     */
+    private void drawLine(int tickLength, int label) {
+        for (int i = 0; i < tickLength; i++)
+            System.out.print("-");
+        if (label >= 0)
+            System.out.print(" " + label);
+        System.out.print("\n");
     }
 
-    private static void drawLine(int tickLength){
-        drawLine(tickLength, - 1);
+    /**
+     * Rysuje linie o podanej dlugosci bez etykiety
+     */
+    private void drawLine(int tickLength) {
+        drawLine(tickLength, -1);
     }
 }
